ac55c2384253c2adfa2e3c9b7ffcae76a8a2cad0
Hazhir2002 <Hazhiryousefi2002@gmail.com>
2025-01-05 16:41:29 +0000

    Deploying to gh-pages from @ Hazhir2002/hazhir2002.github.io@30e03df90f8191b2cc56ac34e77774ac0538ac42 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,7 +457,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%48%61%7A%68%69%72%59%6F%75%73%65%66%69%32%30%30%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -485,7 +485,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://hazhir2002.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
